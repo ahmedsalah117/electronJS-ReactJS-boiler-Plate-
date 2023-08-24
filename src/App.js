28fc015b4cf37b232252978fcdc3a79d430cb512
Ahmed Bahnasy <ahmedbahnasy1171998@gmail.com>
2023-08-24 15:04:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./Components/Login/Login.jsx";
+
+function App() {
+  return (
+    <>
+      <h1 className="main-header">empty header</h1>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./login.module.css";
+function Login() {
+  return <h1 className={classes["main-header"]}>This is the Login</h1>;
+}
+
+export default Login;
